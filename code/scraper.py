e84984f79a8cc526484a6bd3a9fb2a6a770721d5
mikeyy <adhikari.shrish1@gmail.com>
2022-09-02 23:26:28 +0545

    More efficient way of scraping twitter

--- code/scraper.py
+++ code/scraper.py
@@ -3,11 +3,12 @@ import argparse
 import requests
 from bs4 import BeautifulSoup as bs
 import os 
-from dotenv import load_dotenv
+import urllib
 
-load_dotenv()
+BEARER_TOKEN = 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA'
+URL_ID = "https://api.twitter.com/1.1/guest/activate.json"
+URL = 'https://twitter.com/i/api/graphql/gr8Lk09afdgWo7NvzP89iQ/UserByScreenName'
 
-BEARER_TOKEN = os.getenv("BEARER_TOKEN")
 
 headers = {
     "Authorization": f"Bearer {BEARER_TOKEN}"
@@ -24,17 +25,48 @@ def get_handle():
     return args.handle
 
 
+def get_headers():
+    headers = {
+        'authorization': BEARER_TOKEN
+    }
+    id = requests.post(URL_ID, headers= headers).json()['guest_token']
+    
+    headers = {
+        "authorization": BEARER_TOKEN,
+        "x-guest-token": id,
+    }
+    return headers
 
-def scrap_bio(url):
-    r = requests.get(url, headers=headers).json()
+def get_params(handle):
+    
+    # val = f'{"screen_name":{handle},"withSafetyModeUserFields":true,"withSuperFollowsUserFields":true}'
+    # e_val  = urllib.parse.quote(val.encode('utf8'))
+
+    # params = {
+    #     "variables": f"%7B%22screen_name%22%3A%22{handle}%22%2C%22withSafetyModeUserFields%22%3Atrue%2C%22withSuperFollowsUserFields%22%3Atrue%7D"
+    # }
+    # params = {
+    #     "variables": {
+    #         "screen_name": handle
+    #     }
+    # }
+    params = f"variables=%7B%22screen_name%22%3A%22{handle}%22%2C%22withSafetyModeUserFields%22%3Atrue%2C%22withSuperFollowsUserFields%22%3Atrue%7D"
+    # print(params)
+    return params
+                                
+def scrap_bio(headers, params):
+    # params = {'screen_name': 'elonmusk'}
+    # r = requests.get(URL, headers=headers, params=params).json()
+    r = requests.get(URL + f"?{params}", headers=headers).json()
     # print(r)
-    if r['errors']:
+    if r['data']['user']['result']['__typename'] == 'UserUnavailable':
         return "No such user"
-    return r['data'][0]['description']
+    return r['data']['user']['result']['legacy']['description']
     
 
 if __name__ == "__main__":
-    handle = get_handle()
-    url = f"https://api.twitter.com/2/users/by?user.fields=description&usernames={handle}"    
-    print(scrap_bio(url))
+    handle = get_handle().lower()
+    headers = get_headers()
+    params = get_params(handle) 
+    print(scrap_bio(headers, params))
     
