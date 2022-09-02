9198e6fb4cec98adccceadb8bfc3429ebe9c8f54
mikeyy <adhikari.shrish1@gmail.com>
2022-09-02 16:09:44 +0545

    Found the bio

--- code/scraper.py
+++ code/scraper.py
@@ -6,7 +6,7 @@ import argparse
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="A twitter bio scrapper")
+    parser = argparse.ArgumentParser(description="A twitter bio scraper")
     parser.add_argument("--handle", help="Twitter handle without the `@`")
     args = parser.parse_args()
     print(args.handle)
